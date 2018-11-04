9d0c402ac0d911a756fdf6dd4d4a96c8229f085d
edudevjob <edudeveloperctk@gmail.com>
2018-11-04 11:35:34 -0200

    Create Branch createLogin

--- blogLaraVue/resources/views/home.blade.php
+++ blogLaraVue/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Você está Logado!
                 </div>
             </div>
         </div>
