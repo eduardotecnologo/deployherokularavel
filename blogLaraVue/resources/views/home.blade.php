53c10e05669a8b2578925fc1f8c940a283a1a8c4
edudevjob <edudeveloperctk@gmail.com>
2018-11-04 13:51:09 -0200

    Create Panel Home

--- blogLaraVue/resources/views/home.blade.php
+++ blogLaraVue/resources/views/home.blade.php
@@ -2,22 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Você está Logado!
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Dashboard">Teste de Conteúdo...
+            <div class="row">
+                <div class="col-md-4">
+                    <painel titulo="Conteúdo 1">
+                        Teste de Conteúdo 1....
+                    </painel> 
                 </div>
+                <div class="row">
+                <div class="col-md-4">
+                    <painel titulo="Conteúdo 2">
+                        Teste de Conteúdo 2....
+                    </painel> 
+                </div>
+                <div class="row">
+                <div class="col-md-4">
+                    <painel titulo="Conteúdo 3">
+                        Teste de Conteúdo 3....
+                    </painel> 
+                </div>
+                </painel>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- blogLaraVue/resources/views/layouts/app.blade.php
+++ blogLaraVue/resources/views/layouts/app.blade.php
@@ -15,61 +15,36 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+    <!--Chamando o Componente Topo-->
+    <topo titulo="{{ config('app.name', 'Laravel') }}" url="{{ url('/') }}">
+        @guest
+             <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Novo Usuário</a></li>
+        @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        Sair
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        @endguest
+    </topo>
+    <!--Chamando o Componente Topo-->
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+       
 
         @yield('content')
     </div>
