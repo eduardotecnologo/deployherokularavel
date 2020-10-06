173dd08b2ce17cdb88723ec68283f83deedf6e75
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-10-06 00:29:45 -0300

    Create component Dashboard

---
+++ vue/productTest/frontend/src/pages/Dashboard/DashboardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <div class="sidebar">
+
+    </div>
+    <div class="content-right">
+      <div class="navbar">
+        s
+      </div>
+      <div class="pages">
+        o
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'DashboardComponent',
+}
+</script>
+
+<style
+  lang="scss"
+  src="./style.scss"
+  scoped />
